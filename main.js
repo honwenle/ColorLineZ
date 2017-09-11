f2e8aa8b31b3bc11e573b2df0cb0e37630ffcd86
洪文乐 <noclip@foxmail.com>
2017-09-11 09:03:56 +0800

    画背景

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 var cvs = document.getElementById('cvs');
 var ctx = cvs.getContext('2d');
+var WRAP_SIZE = 298;
+var SIZE = 32;
+cvs.width = WRAP_SIZE;
+cvs.height = WRAP_SIZE;
+
+function drawBack () {
+    ctx.fillStyle = '#bbb';
+    ctx.beginPath();
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+            ctx.fillRect(j+1 + SIZE*j, i+1 + SIZE*i, SIZE, SIZE);
+        }
+    }
+    ctx.closePath();
+    ctx.fill();
+}
+drawBack();
